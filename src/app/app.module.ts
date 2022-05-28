49ab9a105a5187385febd8921e367582c2f6bf46
shagat <shagat.bing@gmail.com>
2022-05-28 18:27:35 -0400

    game-odd and even using ng content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { OddComponent } from './odd/odd.component';
+import { EvenComponent } from './even/even.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    OddComponent,
+    EvenComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/game/game.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+//import { clearInterval } from 'timers';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.css']
+})
+export class GameComponent implements OnInit {
+  @Output() gameTimerStarts = new EventEmitter<{ startGameTime: number }>();
+  gameTimer: Array<number> = [0];
+  i = 0;
+  timeoutId: null | ReturnType<typeof setTimeout> = null
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onClickStart() {
+    this.timeoutId = setInterval(() => {
+      this.i += 1
+      this.gameTimer.push(this.i)
+    }, 1000)
+  }
+
+  checkModulus(gt: number) {
+    if (gt % 2 == 0) {
+      return true
+    }
+    else {
+      return false
+    }
+  }
+
+  onClickStop() {
+    if (this.timeoutId) {
+      clearInterval(this.timeoutId)
+    }
+  }
+}
+
