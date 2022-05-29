7edeca6d768bff5c969ee1dac11418cbab9b7746
shagat <shagat.bing@gmail.com>
2022-05-29 06:53:34 -0400

    cleared the previous game control codes and commented it to implement the same with diffrent logic

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -8,35 +8,35 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class GameComponent implements OnInit {
   @Output() gameTimerStarts = new EventEmitter<{ startGameTime: number }>();
-  gameTimer: Array<number> = [0];
-  i = 0;
-  timeoutId: null | ReturnType<typeof setTimeout> = null
+  // gameTimer: Array<number> = [0];
+  // i = 0;
+  // timeoutId: null | ReturnType<typeof setTimeout> = null
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  onClickStart() {
-    this.timeoutId = setInterval(() => {
-      this.i += 1
-      this.gameTimer.push(this.i)
-    }, 1000)
-  }
+  // onClickStart() {
+  //   this.timeoutId = setInterval(() => {
+  //     this.i += 1
+  //     this.gameTimer.push(this.i)
+  //   }, 1000)
+  // }
 
-  checkModulus(gt: number) {
-    if (gt % 2 == 0) {
-      return true
-    }
-    else {
-      return false
-    }
-  }
+  // checkModulus(gt: number) {
+  //   if (gt % 2 == 0) {
+  //     return true
+  //   }
+  //   else {
+  //     return false
+  //   }
+  // }
 
-  onClickStop() {
-    if (this.timeoutId) {
-      clearInterval(this.timeoutId)
-    }
-  }
+  // onClickStop() {
+  //   if (this.timeoutId) {
+  //     clearInterval(this.timeoutId)
+  //   }
+  // }
 }
 
